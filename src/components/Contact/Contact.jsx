df6ac2ab9eb2a6b35e1c37a7eee03fefe96f8393
AntonSizov <sizov.anton.v@gmail.com>
2024-02-20 21:29:57 +0200

    update delete

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,13 @@
 import { FaPhone } from "react-icons/fa";
 import { MdOutlineMan } from "react-icons/md";
-import css from './Contact.module.css'
-import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contactSlice";
+import css from './Contact.module.css';
 
-export const Contact = ({id, name, number }) => {
-  const dispatch = useDispatch();
-  const handleDelete = ()=> dispatch(deleteContact(id));
-  
+
+export const Contact = ({ name, number }) => {  
   return (
     <div className={css.contact_wrapper}>
       <p className={css.contact_field}><MdOutlineMan className={ css.contact_icon} /> {name}</p>
       <p className={css.contact_field}> <FaPhone className={css.phone_icon} /> {number}</p>
-      <button type="button" onClick={handleDelete}>Delete</button>
    </div>
  )
 };
